ba9715a765e00de892059618d12aed17d6389734
Kebiin <kevincuadrado2017@gmail.com>
2025-03-08 08:57:03 -0500

    Primer commit - Subiendo proyecto

---
+++ ClienteTcpImc/src/yo/imc/cliente/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yo.imc.cliente;
+
+import yo.imc.cliente.vistas.VentanaPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+
+}
---
+++ ServidorTcpImc/src/yo/Imc/principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yo.Imc;
+
+import yo.Imc.vistas.VentanaPrincipal;
+
+public class principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+
+}
